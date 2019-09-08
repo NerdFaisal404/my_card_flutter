7a5a144f5104e9de49744472b7480da9780dd656
Faisal Ahmed <faisal.ahmedbcse@gmail.com>
2019-09-08 18:23:43 +0600

    my card design done

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,11 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50,
-               backgroundImage: AssetImage('images/faisal.jpeg'),
+                backgroundImage: AssetImage('images/faisal.jpeg'),
               ),
               Text(
                 'Faisal Ahmed',
@@ -28,6 +29,50 @@ class MyApp extends StatelessWidget {
                   fontSize: 40,
                   fontWeight: FontWeight.bold,
                   fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'Mobile App Developer',
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.teal),
+                  title: Text(
+                    '+88017848483',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(Icons.email, color: Colors.teal),
+                  title: Text(
+                    'imdevfaisal@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontSize: 20,
+                    ),
+                  ),
                 ),
               ),
             ],
